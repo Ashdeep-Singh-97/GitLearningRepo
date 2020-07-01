1870cd6a54c0d36c722c76b7cd36f9b04a77e9c5
Ashdeep Singh <royal_rebellion@J.A.R.V.I.S>
2020-07-01 21:30:58 +0530

    updated for loops with man ki baat

--- forloops.py
+++ forloops.py
@@ -1,3 +1,7 @@
 for i in range(10):
     print("Hi")
+print("This is for loop")
+print("I am not a loser")
+print("There must be someone who will love me")
+print("And for that one i will win the whole world")
 
